a510169d02c9e3834833af753710903e037d89a9
nghiach <chnghia@gmail.com>
2020-07-20 17:04:38 +0700

    integrate pix2pix with pytorch-lightning

---
+++ models/pix2pix_model.py
@@ -0,0 +1,139 @@
+import os
+from argparse import ArgumentParser, Namespace
+from collections import OrderedDict
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torchvision.datasets import MNIST
+
+from pytorch_lightning.core import LightningModule
+from pytorch_lightning.trainer import Trainer
+from pix2pix.models import GeneratorUNet, Discriminator
+
+cuda = True if torch.cuda.is_available() else False
+# Tensor type
+Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+
+
+class Pix2PixModel(LightningModule):
+    def __init__(self,
+                 latent_dim: int = 100,
+                 lr: float = 0.0002,
+                 b1: float = 0.5,
+                 b2: float = 0.999,
+                 batch_size: int = 64,
+                 img_height: int = 256,
+                 img_width: int = 256, ** kwargs):
+        """Initialize the pix2pix class.
+
+        Parameters:
+            opt (Option class)-- stores all the experiment flags; needs to be a subclass of BaseOptions
+        """
+        super().__init__()
+        self.lr = lr
+        self.b1 = b1
+        self.b2 = b2
+        self.batch_size = batch_size
+        self.img_height = img_height
+        self.img_width = img_width
+
+        self.generator = GeneratorUNet()
+        self.discriminator = Discriminator()
+
+        # Loss functions
+        self.criterion_GAN = torch.nn.MSELoss()
+        # Calculate output of image discriminator (PatchGAN)
+        self.patch = (1, img_height // 2 ** 4, img_width // 2 ** 4)
+        self.criterion_GAN = torch.nn.MSELoss()
+        self.criterion_pixelwise = torch.nn.L1Loss()
+
+   def forward(self, z):
+        raise NotImplementedError
+
+    def adversarial_loss(self, y_hat, y):
+        raise NotImplementedError
+
+    def training_step(self, batch, batch_idx, optimizer_idx):
+        imgs, _ = batch
+
+        # Model inputs
+        real_A = Variable(batch["B"].type(Tensor))
+        real_B = Variable(batch["A"].type(Tensor))
+
+        # Adversarial ground truths
+        valid = Variable(
+            Tensor(np.ones((real_A.size(0), *self.patch))), requires_grad=False)
+        fake = Variable(
+            Tensor(np.zeros((real_A.size(0), *self.patch))), requires_grad=False)
+
+        # train generator
+        if optimizer_idx == 0:
+            # generate images
+            self.optimizer_G.zero_grad()
+            # GAN loss
+            fake_B = self.generator(real_A)
+            pred_fake = self.discriminator(fake_B, real_A)
+            loss_GAN = self.criterion_GAN(pred_fake, valid)
+            # Pixel-wise loss
+            loss_pixel = self.criterion_pixelwise(fake_B, real_B)
+
+            # Total loss
+            g_loss = loss_GAN + lambda_pixel * loss_pixel
+            g_loss = 0
+            tqdm_dict = {'g_loss': g_loss}
+
+            output = OrderedDict({
+                'loss': g_loss,
+                'progress_bar': tqdm_dict,
+                'log': tqdm_dict
+            })
+            return output
+
+        # train discriminator
+        if optimizer_idx == 1:
+            # Measure discriminator's ability to classify real from generated samples
+            self.optimizer_D.zero_grad()
+
+            # Real loss
+            pred_real = self.discriminator(real_B, real_A)
+            loss_real = self.criterion_GAN(pred_real, valid)
+
+            # Fake loss
+            pred_fake = self.discriminator(fake_B.detach(), real_A)
+            loss_fake = self.criterion_GAN(pred_fake, fake)
+
+            # Total loss
+            d_loss = 0.5 * (loss_real + loss_fake)
+            tqdm_dict = {'d_loss': d_loss}
+            output = OrderedDict({
+                'loss': d_loss,
+                'progress_bar': tqdm_dict,
+                'log': tqdm_dict
+            })
+            return output
+
+        raise NotImplementedError
+
+    def configure_optimizers(self):
+        lr = self.lr
+        b1 = self.b1
+        b2 = self.b2
+
+        # Optimizers
+        opt_g = torch.optim.Adam(
+            self.generator.parameters(), lr=lr, betas=(b1, b2))
+        opt_d = torch.optim.Adam(
+            self.discriminator.parameters(), lr=lr, betas=(b1, b2))
+        return [opt_g, opt_d], []
+
+    def train_dataloader(self):
+        raise NotImplementedError
+
+    def on_epoch_end(self):
+        raise NotImplementedError
---
+++ train.py
@@ -0,0 +1,49 @@
+import os
+from argparse import ArgumentParser, Namespace
+from collections import OrderedDict
+from models.pix2pix import Pix2PixModel
+from pytorch_lightning.trainer import Trainer
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from PIL import Image
+from models.pix2pix.datasets import ImageDataset
+
+
+def main(img_height: int = 256,
+         img_width: int = 256,
+         dataset_name="",
+         batch_size: int = 1,
+         n_cpu: int = 8) -> None:
+
+    # Configure dataloaders
+    transforms_ = [
+        transforms.Resize((img_height, img_width), Image.BICUBIC),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ]
+
+    dataloader = DataLoader(
+        ImageDataset("./data/%s" %
+                     dataset_name, transforms_=transforms_),
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=n_cpu,
+    )
+
+    val_dataloader = DataLoader(
+        ImageDataset("./data/%s" % dataset_name,
+                     transforms_=transforms_, mode="val"),
+        batch_size=10,
+        shuffle=True,
+        num_workers=1,
+    )
+
+    model = Pix2PixModel()
+
+    trainer = Trainer(gpus=[0])
+
+    trainer.fit(model, dataloader, val_dataloader)
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
